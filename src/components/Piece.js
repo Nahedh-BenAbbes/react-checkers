4bb7c128cfa1eca7af52848e53a77752e379b26c
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-20 01:55:31 -0400

    Installed redux and created boilerplate for Piece class

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -2,7 +2,54 @@
 
 import React, { Component } from 'react';
 import '../css/Piece.css';
+import { connect } from 'react-redux';
 
 class Piece extends Component {
-    
+
+    // Remove game piece when overtaken
+    removePiece = () => {
+        return this.props.removePiece()
+    }
+
+    // Mark piece as king
+    king = () => {
+        return this.props.king()
+    }
+
+    // Get available spaces to move based on color/current row and column
+    getAvailableMove = () => {
+        return this.props.getAvailableMove()
+    }
+
+    render = () => {
+        return (
+            <div className="rounded-circle"></div>
+        )
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        active: true,
+        isKing: false,
+        color,
+        currentRow,
+        currentColumn
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removePiece: function() {
+            dispatch({ type: 'REMOVEPIECE' });
+        },
+        king: function() {
+            dispatch({ type: 'KING' });
+        },
+        getAvailableMove: function() {
+            dispatch({ type: 'GETAVAILABLEMOVE' })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Piece);
